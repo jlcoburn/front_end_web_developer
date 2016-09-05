bb618f2b92b920b894052815a5431276202335bd
Han Jiang <ptchiang12@gmail.com>
2016-09-04 20:52:34 -0700

    adding gems

--- arcade_game1/js/app.js
+++ arcade_game1/js/app.js
@@ -4,9 +4,9 @@ var Entity = function() {
   this.x = 0;
   this.y = 0;
 };
-
+var posY = [60, 140, 210];
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -14,9 +14,10 @@ var Enemy = function(x, y, speed) {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     // coordinates on the canvas
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
+    var speeds = [50, 70, 100, 120, 150];
+    this.x = -100;
+    this.y = posY[Math.floor(Math.random() * 3)];
+    this.speed = speeds[Math.floor(Math.random() * 5)];
 };
 
 // Update the enemy's position, required method for game
@@ -49,7 +50,6 @@ Enemy.prototype.checkCollisions = function(){
        player.reset();
        player.life -= 1;
   }
-  document.getElementById('score').innerHTML = "SCORE: " + this.score;
 }
 
 // Now write your own player class
@@ -57,12 +57,7 @@ Enemy.prototype.checkCollisions = function(){
 // a handleInput() method.
 
 var Player = function() {
-  this.Boy = 'images/char-boy.png';
-  this.catGirl = 'images/char-cat-girl.png';
-  this.hornGirl = 'images/char-horn-girl.png';
-  this.pinkGirl = 'images/char-pink-girl.png';
-  this.princessGirl = 'images/char-princess-girl.png';
-  this.player = 'images/char-boy.png';
+  this.playerImage = 'images/char-boy.png';
   // coordinates on the canvas
   this.x = 200;
   this.y = 400;
@@ -98,7 +93,7 @@ Player.prototype.update = function() {
 };
 
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.player), this.x, this.y);
+    ctx.drawImage(Resources.get(this.playerImage), this.x, this.y);
 };
 
 Player.prototype.handleInput = function(userInput) {
@@ -127,12 +122,45 @@ Player.prototype.reset = function(){
   this.y = 400;
 }
 
+var gemImages = ['images/Gem-Orange.png', 'images/Gem-Blue.png', 'images/Gem-Green.png'];
+var Gem = function() {
+  this.gemPosX = [50, 150, 250, 350, 450];
+  this.gemImg = gemImages[Math.floor(Math.random() * 3)];
+  this.x = this.gemPosX[Math.floor(Math.random() * 5)];
+  this.y = posY[Math.floor(Math.random() * 3)];
+};
+
+Gem.prototype.update = function(){
+  this.checkCollisions();
+}$
+
+Gem.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.gemImg), this.x, this.y);
+};
+
+Gem.prototype.checkCollisions = function(){
+  // calculate the bounding box objects' value
+  var rect1 = {x: player.x, y: player.y, width: 50, height: 50}
+  var rect2 = {x: this.x, y: this.y, width: 50, height: 50}
+
+  if (rect1.x < rect2.x + rect2.width &&
+     rect1.x + rect1.width > rect2.x &&
+     rect1.y < rect2.y + rect2.height &&
+     rect1.height + rect1.y > rect2.y) {
+       this.gemImg = gemImages[Math.floor(Math.random() * 3)];
+       this.x = this.gemPosX[Math.floor(Math.random() * 5)];
+       this.y = posY[Math.floor(Math.random() * 3)];
+       player.score += 1;
+  }
+  document.getElementById('score').innerHTML = "SCORE: " + player.score;
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
 var player = new Player();
-var allEnemies = [new Enemy(0, 60, 70), new Enemy(0, 140, 30), new Enemy(0, 210, 120)];
+var allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy()];
+var gem = new Gem();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
