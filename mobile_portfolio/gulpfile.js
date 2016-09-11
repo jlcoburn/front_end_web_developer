79334a31aea23ff8337fb91bc157d5982231bd69
chloe <chloe@hanyus-MacBook-Pro.local>
2016-09-11 16:11:00 -0700

    inline css

---
+++ mobile_portfolio/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var critical = require('critical');
+
+gulp.task('critical', ['build'], function (cb) {
+    critical.generate({
+        inline: true,
+        base: 'dist/',
+        src: 'index.html',
+        dest: 'dist/index-critical.html',
+        minify: true,
+        width: 320,
+        height: 480
+    });
+});
